05e16df2a0a5ddd00cb5c330b55e07667474a781
moogieon <vzlzydn5007@gmail.com>
2021-10-09 22:58:44 +0900

    todo list test

--- my-learn/src/todolist/todoList.styles.ts
+++ my-learn/src/todolist/todoList.styles.ts
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const Wrapper=styled.div`
 width: 100%;
 /* height: 100pc; */
-background-color: green;
+background-color: lightgreen;
 display: flex;
 flex-direction: column;
 
