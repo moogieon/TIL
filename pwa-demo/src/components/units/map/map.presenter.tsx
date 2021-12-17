1bc0808d27cadcec7597e98cf62d1ea5739e4357
moogieon <vzlzydn5007@gmail.com>
2021-12-18 01:32:32 +0900

    add-map css

---
+++ pwa-demo/src/components/units/map/map.presenter.tsx
@@ -0,0 +1,22 @@
+import { Wrapper, Head, Body, Body_Head } from "./map.styles";
+
+export default function MapUI() {
+  return (
+    <>
+      <Wrapper>
+        <Head>My Portfolio</Head>
+        <Body>
+          <Body_Head>
+            <ul>
+              <li>Shop</li>
+              <li> 헤이고</li>
+              <li>TodoList</li>
+              <li>반응형 웹사이트</li>
+              <li>실서비스</li>
+            </ul>
+          </Body_Head>
+        </Body>
+      </Wrapper>
+    </>
+  );
+}
--- pwa-demo/src/components/units/map/map.styles.ts
+++ pwa-demo/src/components/units/map/map.styles.ts
@@ -7,9 +7,3 @@ export const Wrapper = styled.div`
 export const Head = styled.div``;
 export const Body = styled.div``;
 export const Body_Head = styled.div``;
-export const EmailBox = styled.div``;
-export const EmailHead = styled.div``;
-export const EmailBody = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
