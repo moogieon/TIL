118ab6bf70cf30299df44600b7a925e8a7adcb59
moogieon <vzlzydn5007@gmail.com>
2021-12-19 23:43:53 +0900

    add-map css

--- pwa-demo/src/components/units/map/map.presenter.tsx
+++ pwa-demo/src/components/units/map/map.presenter.tsx
@@ -4,17 +4,9 @@ export default function MapUI() {
   return (
     <>
       <Wrapper>
-        <Head>My Portfolio</Head>
+        <Head>map</Head>
         <Body>
-          <Body_Head>
-            <ul>
-              <li>Shop</li>
-              <li> 헤이고</li>
-              <li>TodoList</li>
-              <li>반응형 웹사이트</li>
-              <li>실서비스</li>
-            </ul>
-          </Body_Head>
+          <Body_Head></Body_Head>
         </Body>
       </Wrapper>
     </>
